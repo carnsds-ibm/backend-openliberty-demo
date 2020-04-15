4eff85eac107c5ba4cf2a90b59253e5ef5f6b82f
Dillon Carns <Dillon.Carns@ibm.com>
2020-04-15 14:15:37 -0400

    Fixed up some yaml to add more service mesh usage

---
+++ k8s_yamls/curler.sh
@@ -0,0 +1,5 @@
+while [ true ]
+do
+ curl -v http://istio-ingressgateway-istio-system.cpat-dev-sandbox-ocp435-afb9c6047b062b44e3f1b3ecfeba4309-0000.sjc03.containers.appdomain.cloud/
+ sleep 0.2
+done
